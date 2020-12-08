6af3b4cc5f026ee592d3ef1827a71f51f496a10c
sunghyunchoi <chltjdgus99@naver.com>
2020-12-08 16:51:30 +0900

    Sunghyun

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request,session
+from flask import Flask, render_template, jsonify, request, session, escape
 import requests
 from bs4 import BeautifulSoup
 from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
@@ -9,6 +9,8 @@ app = Flask(__name__)
 client = MongoClient('mongodb://test:test@54.180.91.148',27017)
 #db = client.dbsparta  # 'dbsparta'라는 이름의 db를 만들거나 사용합니다.
 db = client.jungglebook
+app.secret_key = b"!@#$1234"
+
 
 @app.route('/')
 def home():
@@ -17,7 +19,7 @@ def home():
 
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/signup', methods=['POST'])
 def signUp():
     # id_receive = request.form['id']  # 클라이언트로부터 id을 받는 부분
     # password_receive = request.form['password']  # 클라이언트로부터 password 받는 부분
@@ -29,8 +31,48 @@ def signUp():
 
 
 
-@app.route('/login', methods=['GET'])
+@app.route('/session', methods=['POST', 'GET'])
+def session_test():
+    result = "";
+    if request.method == "GET":
+        if "username" in session:
+            print("유지중")
+            result = "유지중"
+        else:
+            print("세션없음")
+            result = "세션없음"
+
+        return result
+
+    else:
+        user_id = request.form['id_give']
+        user_password = request.form['password_give']
+
+        user = db.userDB.find_one({'id': user_id, 'password': user_password})
+        if user:
+
+            if "username" in session:
+                print("유지중")
+                result = escape(session['username'])
+            else:
+                print("세션없음")
+                session['username'] = request.form['name']
+                result = escape(session['username'])
+
+            return render_template("main.html")
+
+        else:
+
+            return jsonify({"result":"Fail"})
+
+
+
+
+
+
+@app.route('/main', methods=['POST'])
 def checkUser():
+
     result = list(db.userDB.find({}, {'_id': 0}))
    
 
