8d0030616b701d99ab42ff27e3657f8d6ce411cd
zeroistfilm <zeroistfilm@naver.com>
2020-12-08 10:55:44 +0900

    test

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
+    print('s')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
