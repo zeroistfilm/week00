ce65a324b47049ace30531c196f16f00277dcb4a
zeroistfilm <zeroistfilm@naver.com>
2020-12-08 10:56:27 +0900

    test2

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
     print('s')
+    print('asldkjalskdjaslkdjalkdjalksjd')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
