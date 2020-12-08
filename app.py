d105a6ceb5cdf7274ebe7d170015548a3b7d1ef7
zeroistfilm <zeroistfilm@naver.com>
2020-12-08 14:29:55 +0900

    change db information for AWS

--- app.py
+++ app.py
@@ -7,11 +7,32 @@ app = Flask(__name__)
 
 #client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
 client = MongoClient('mongodb://test:test@54.180.91.148',27017)
-db = client.dbsparta  # 'dbsparta'라는 이름의 db를 만들거나 사용합니다.
-
+#db = client.dbsparta  # 'dbsparta'라는 이름의 db를 만들거나 사용합니다.
+db = client.jungglebook
 
 @app.route('/')
 def home():
+
+    #signup
+    id_receive = 'zeroistfilm' #변수로 변경해야함
+    password_receive = '1234'
+    userName_receive = '김영동'
+    userState_receive = '운영진' #or 1기, 2기 ...
+
+    userInfo = {'id': id_receive,
+                'password': password_receive,
+                'username':userName_receive,
+                'userState':userState_receive}
+
+    # 3. mongoDB에 데이터를 넣기
+    db.userDB.insert_one(userInfo)
+
+    # check user
+
+
+
+
+
     return render_template('index.html')
 
 
@@ -51,6 +72,24 @@ def read_articles():
     # 2. articles라는 키 값으로 article 정보 보내주기
     return jsonify({'result': 'success', 'articles': result})
 
+
+
+@app.route('/login', methods=['POST'])
+def signUp():
+    # id_receive = request.form['id']  # 클라이언트로부터 id을 받는 부분
+    # password_receive = request.form['password']  # 클라이언트로부터 password 받는 부분
+    id_receive = 'zeroistfilm'
+    password_receive = '1234'
+    userInfo = {'id': id_receive, 'password': password_receive}
+    print('a')
+    # 3. mongoDB에 데이터를 넣기
+    db.userDB.insert_one(userInfo)
+
+
+@app.route('/login', methods=['GET'])
+def checkUser():
+    result = list(db.userDB.find({}, {'_id': 0}))
+   
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
